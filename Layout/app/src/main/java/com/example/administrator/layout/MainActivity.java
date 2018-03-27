d93bbabbadc3cff8dc66f99e5cfa746ec78e1f52
ATN-MU <1249220979@qq.com>
2018-03-27 19:51:10 +0800

    Layout

---
+++ Layout/app/src/main/java/com/example/administrator/layout/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        linearLayout
+//        setContentView(R.layout.first_linearlayout);
+//        relativeYayout
+//        setContentView(R.layout.relativelayout);
+//        tableYayout
+        setContentView(R.layout.tablelayout);
+    }
+}
